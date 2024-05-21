2a2ace97e6815c80aa32b47b914748381c80c4ed
AABDOUAA <abdelkawyabdou@gmail.com>
2024-05-21 13:50:29 +0100

    Update dataPrep.py

--- dataPrep.py
+++ dataPrep.py
@@ -37,8 +37,8 @@ def dfCollabs(basedf, uniName):
   a = collabList.to_numpy()
   title = basedf['Title']
   dfCollabs = pd.DataFrame({'Institutions': collabList.keys(), 'Occurences': a, 'Title': title})
-
-return dfCollabs
+  
+  return dfCollabs
 
 # this returns a datframe of the set of collaborations that a university has been involved in within the dataset
 # each having an occurence of 1 - ie this is the unpacked form of the previous function
